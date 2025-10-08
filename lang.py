e628dc5f27a9be5143a2ec324ff78a51a3b0a323
LB6283 <lucabeltran222@gmail.com>
2025-10-08 19:43:02 -0400

    Update lang.py

--- lang.py
+++ lang.py
@@ -6,7 +6,7 @@ def out(*args, **kwargs):
             print(arg, end=kwargs.get('end_spaceing', ''))
         else:
             print(arg, end=kwargs.get('spaceing', ''))
-    print(kwargs.get('end', '\n'))
+    print(end=kwargs.get('end', '\n'))
 
 
 def accept(*args, **kwargs):
@@ -20,19 +20,127 @@ def accept(*args, **kwargs):
     print(end=kwargs.get('cursor_position', ''))
     y = input()
     return y
-    
+   
 def length(x):
-    return len(x)
+    counter = 0
+    for y in x:
+        counter += 1
+    return counter
 
 def final_index(x):
-    return (len(x) - 1)
+    return (length(x) - 1)
 def last_item(x):
     item = x[final_index(x)]
     return item
-def n_to_last(x, n):
+def rev_index(x, n):
     item = x[final_index(x) - n]
+    return item
 
-# Test
+def sum(*args):
+    result = Num(0)
+    for arg in args:
+        result += arg
+    return result
 
-fruits = ['apple', 'banana', 'pineapple', 'orange', 'blueberry']
-print(n_to_last(fruits, 6))
+def diff(*args):
+    result = 0
+    for arg in args:
+        result -= arg
+    return result
+
+def product(*args):
+    result = 0
+    for arg in args:
+        result *= arg
+    return result
+
+def quot(*args):
+    result = 0
+    for arg in args:
+        result /= arg
+    return result
+
+
+
+
+
+
+############################################################################################################################################
+################################################################## CLASSES: ################################################################
+############################################################################################################################################
+
+
+
+
+
+class List:
+    def __init__(self, listofvalues):
+        self.listofvalues = listofvalues
+    
+    def make_list(self):
+        return self.listofvalues;
+    
+    def length(self):
+        return len(self.make_list())
+    
+    def values(self):
+        items = self.listofvalues
+        def recursive(x):
+            entries = []
+            for item in x:
+                if isinstance(item, (str, bool, int, float)):
+                    entries.append(item)
+                elif isinstance(item, (dict, list, set, tuple)): 
+                    entries.extend(recursive(item))
+            return entries
+        return recursive(items)
+    
+    def __str__(self):
+        return f"List({self.make_list()})"
+
+
+# DATA TYPES:
+
+
+class String:
+    def __init__(self, string):
+        if not isinstance(string, str):
+            raise ValueError (f"Could not interpret {type(string)} as <class 'String'>")
+        else:
+            self.string = string
+
+    def __str__(self):
+        return self.make_string()
+    
+    def make_string(self):
+        return self.string
+
+
+class Num:
+    def __init__(self, num):
+        if not isinstance(num, (int, float)):
+            raise ValueError (f"Could not interpret {type(num)} as <class 'Num'>")
+        else:    
+            self.num = num
+    
+    def __str__(self):
+        return f"{self.number()}"
+
+    def __add__(self, other):
+        return Num(self.number() + other.number())
+
+    def number(self):
+        return self.num
+
+## Variable declaration in mamba:
+
+class Var:
+    def __init__(self):
+        self.vars = {}
+    def declare(self, name, value):
+        self.vars[name] = value
+    def fetch(self, index):
+        return self.vars[index]
+
+
+variables = Var()
